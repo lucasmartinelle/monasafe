5ccb59cca7a5ba77c9dfb0b7c557dd146a9a81b9
lucas MARTINELLE <lucasmar.martinelle@gmail.com>
2026-02-15 15:19:57 +0100

    fix: correction dart analyze

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:monasafe/main.dart';
 
 void main() {
   testWidgets('Monasafe app renders correctly', (WidgetTester tester) async {
